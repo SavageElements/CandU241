2bf97cfdbef13739c0ff6f281872f0db75c615f4
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-07 22:10:07 -0700

    Add project files.

---
+++ CandU241/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandU241
+{
+     public class Employee : Person241
+    {
+        public int ID;
+    }
+}
---
+++ CandU241/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CandU241
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person241 person241 = new Person241();
+            person241.firstName = "Sample";
+            person241.lastName = "Student";
+
+            person241.SayName();
+
+        }
+    }
+}
